f80192cf7ab97cea5d66ff136cb8ea827f056642
ads6495 <astrange55@icloud.com>
2019-09-09 13:57:02 -0400

    finished adventure mode

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace whole_new_world
+{
+  class Program
+  {
+    static void Main(string[] args)
+
+    {
+      var min = 1.0;
+      var max = 100.0;
+      var success = false;
+
+      while (!success)
+      {
+        var guess = Math.Floor((min + max) / 2);
+        Console.WriteLine($"{guess}");
+        var input = Console.ReadLine();
+        if (input == "yes")
+        {
+          success = true;
+          var playAgain = Console.ReadLine();
+
+        }
+        else if (input == "higher")
+        {
+          min = guess;
+
+        }
+        else if (input == "lower")
+        {
+
+          max = guess;
+
+        }
+
+      }
+
+    }
+
+  }
+}
